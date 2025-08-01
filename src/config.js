fcd824b855b6df24eda8615186da580300b0bedc
jai1cop <jai.coppen@gmail.com>
2025-08-01 13:11:07 +0800

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,20 @@
+export const AEMO_API_BASE_URL = "/api/report";
+
+export const PRODUCTION_FACILITIES = [
+    "North West Shelf", "Gorgon", "Wheatstone", "Macedon",
+    "Varanus Island", "Devil Creek", "Pluto", "Xyris Production Facility",
+    "Walyering Production Facility", "Beharra Springs"
+];
+
+export const AEMO_FACILITY_NAME_MAP = {
+    "Karratha Gas Plant": "North West Shelf",
+    "Gorgon Gas Plant": "Gorgon",
+};
+
+export const FACILITY_CAPACITIES = {
+    "North West Shelf": 630, "Gorgon Gas Plant": 300, "Wheatstone": 230,
+    "Macedon": 170, "Varanus Island": 390, "Devil Creek": 50, "Pluto": 40,
+    "Xyris Production Facility": 30, "Walyering Production Facility": 33, "Beharra Springs": 25
+};
+
+export const GSOO_HISTORICAL_DEMAND = { 2022: 1045, 2023: 1078, 2024: 1119 };
